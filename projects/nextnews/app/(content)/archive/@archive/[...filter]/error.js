44e2ffbb9856f73413cba2a813ff25eab7447cda
nohahakim <raafatabass@hotmail.com>
2025-06-26 10:19:05 +0300

    chore: drop duplicate projects/nextnews subtree

--- projects/nextnews/app/(content)/archive/@archive/[...filter]/error.js
+++
@@ -1,9 +0,0 @@
-"use client"; 
-export default function FilterError({ error }) {
-  return (
-    <div id="error">
-      <h2>An Error Occurred</h2>
-      <p>{error.message}</p>
-    </div>
-  );
-}
--- projects/nextnews/app/(content)/news/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getAllNews } from "@/lib/news";
-
-export default async function NewsPage() {
-  const news = await getAllNews();
-
-  return <NewsList news={news} />;
-}
